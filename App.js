4efea301f8a159c6a50b8afa88b2cdf3e08d8d1c
Shah <bhumika.shah@capco.com>
2023-04-09 01:57:09 +0530

    ToDo App

--- App.js
+++ App.js
@@ -1,32 +1,54 @@
 import { useState } from 'react';
-import { Button, FlatList, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, Image, Modal, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  const[textInput, setTextInput] = useState('');
+  const[textInput, setTextInput] = useState();
   const[listOfTodos, setListOfTodos] = useState([]);
+  const [visible, setVisible] = useState(false);
+  const [rand, setRand] = useState(0);
 
   function handleChange(e){
     setTextInput(e);
   }
-  function addTodo(){
-    console.log(textInput);
-    setListOfTodos((currTodo) => [...currTodo, textInput]);
-    console.log(listOfTodos);
+
+  function addGoalHandlder() {
+    setVisible(true);
   }
 
-  function delTodo(){
-    console.log("del");
+  function addTodo(){
+    const min = 1;
+    const max = 100;
+    setRand(min + Math.random() * (max - min));
+    setListOfTodos((currTodo) => [...currTodo, {text: textInput, id: rand}]);
+  }
+
+  function delTodo(id){
+    setListOfTodos((currTodo) => {return currTodo.filter(updatedList => (updatedList.id !== id ))});
+  }
+
+  function back() {
+    setVisible(false);
   }
 
   return (
-    <View style={styles.container}>
-      <TextInput placeholder='enter todo' onChangeText={handleChange}/>
-      <Button title="Add" onPress={addTodo}></Button>
-      {/* scrollview is great if there is limited amount of data because it loads everything all at once*/}
-      <Text>Added Pointers:</Text>
-      <Pressable onPress={delTodo}>
-      {listOfTodos.map(res=>{return <Text>{res}</Text>})}       
-      </Pressable>
+    <View style={styles.buttonstyle} >
+      <Button title="Add New TODO" color="#fff644" onPress={addGoalHandlder} />
+      <Modal visible={visible} animationType='slide'><View style={styles.container}>
+        <TextInput placeholder='enter todo' onChangeText={handleChange}/>
+        <Button title="Add" onPress={addTodo} />
+        <Button title='Back' onPress={back} />
+        {/* scrollview is great if there is limited amount of data because it loads everything all at once*/}
+        <Text>Added Pointers:</Text>
+        {listOfTodos.map(res=>{ 
+          return (
+            <Pressable android_ripple={{color: '#fff644'}} onPress={delTodo.bind(this, res.id)}>
+              <Text key={res.id}>{res.text}</Text>
+            </Pressable>
+          )
+        })}    
+        </View>
+      </Modal>
+      <Image style={styles.image} source={require('./assets/images/mainPageImg.jpg')}/>
     </View>
   );
 }
@@ -38,4 +60,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonstyle: {
+    padding: 50,
+    marginTop: 150,
+  },
+  image: {
+    width: 300,
+    height: 400
+  }
 });
